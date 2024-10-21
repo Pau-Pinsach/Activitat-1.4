44975dbd84c8e09b67383d8fd5aa282790607d3e
Pau Pinsach <pgpp46@gmail.com>
2024-10-21 17:08:25 +0200

    Exercicis fins el 15 completats

--- Ex05/obj/Debug/net8.0/Ex05.AssemblyInfo.cs
+++ Ex05/obj/Debug/net8.0/Ex05.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5edcc39499d9d7bce0966921e8401523cef37dd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d46150306dc0b6787db292c42aec6bf35ec74bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
